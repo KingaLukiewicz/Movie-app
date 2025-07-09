c223973b53171fa16d8a7c108a139f583fac9b69
KingaLukiewicz <kingal@itbs.pl>
2025-07-09 22:37:46 +0200

    main page without data from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js';
+import QuickView from './components/QuickView.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Movie app</h1>
+      <Header></Header>
+      <QuickView text='MOVIES' />
+      <QuickView text='TV SHOWS' />
+      <QuickView text='PEOPLE' />
     </div>
   );
 }
