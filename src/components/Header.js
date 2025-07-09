a4d2d4c7e9090c5943496d5635c234694e3cd8fc
KingaLukiewicz <kingal@itbs.pl>
2025-07-09 22:34:16 +0200

    basic header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+import Searchbar from './Searchbar';
+
+function Header() {
+    return (
+        <div className="Header">
+            <Searchbar></Searchbar>
+        </div>
+    );
+}
+
+export default Header;
