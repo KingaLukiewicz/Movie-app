093cd4200050c248241722178d51473b87d661a6
KingaLukiewicz <kingal@itbs.pl>
2025-07-22 13:41:44 +0200

    carousel component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header.js';
 import QuickView from './components/QuickView.js';
+import Carousel from './components/Carousel';
 
 function App() {
+
   return (
     <div className="App">
       <Header></Header>
-      <QuickView text='MOVIES' />
+      <Carousel text='MOVIES' type='movie' />
       <QuickView text='TV SHOWS' />
       <QuickView text='PEOPLE' />
     </div>
---
+++ src/components/Carousel.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Slider from "react-slick";
+import { useNavigate } from 'react-router-dom';
+import './Carousel.css'
+
+const Carousel = ({ text, type }) => {
+    const API_TOKEN = process.env.REACT_APP_TMDB_TOKEN;
+    const [popular, setPopular] = useState([]);
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+        ],
+    };
+
+    useEffect(() => {
+        const fetchPopular = async () => {
+            try {
+                const response = await axios.get(
+                    `https://api.themoviedb.org/3/trending/${type}/day?language=en-US`, {
+                    headers: {
+                        Authorization: `Bearer ${API_TOKEN}`,
+                        accept: 'application/json'
+                    }
+                });
+                setPopular(response.data.results);
+            }
+            catch (error) {
+                console.error('Failed to fetch', error);
+            }
+        };
+        fetchPopular();
+    }, []);
+
+    return (
+        <div className="Carousel">
+            <div className='Card'>{text}</div>
+            <Slider {...settings}>
+                {popular.map((item) => (
+                    <div
+                        key={item.id}
+                        className='CarouselItem'
+                    >
+                        <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.title} />
+                        {/* <p>{movie.title}</p> */}
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    );
+};
+
+export default Carousel;
