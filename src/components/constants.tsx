37287cb03dd70efd1c21a6764ccfbbb7fdee0b15
KingaLukiewicz <kingal@itbs.pl>
2025-09-01 15:28:12 +0200

    changed backticks to single quotes for consistency

--- src/components/constants.tsx
+++ src/components/constants.tsx
@@ -1,6 +1,6 @@
 export const API_TOKEN = process.env.REACT_APP_TMDB_TOKEN;
 
-export const TMDB_IMAGE_BASE_URL = `https://image.tmdb.org/t/p/w500`;
+export const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 export const TMDB_ID_URL = 'https://api.themoviedb.org/3/';
 
 export const SLIDER_SETTINGS = {
