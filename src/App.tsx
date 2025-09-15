78785118a6da1cccb920c48dae9ef165dff64063
KingaLukiewicz <kingal@itbs.pl>
2025-09-15 21:52:36 +0200

    move header component from Home.tsx to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import MoviePage from './pages/MoviePage';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:movie_id" element={<MoviePage />} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import './Home.css';
-import Header from '../components/Header';
 import Carousel from '../components/Carousel';
 import { CAROUSEL_CATEGORIES } from '../constants';
 
 function Home() {
   return (
     <div className="Home">
-      <Header></Header>
       {Object.entries(CAROUSEL_CATEGORIES).map(([key, { title, type }]) => (
         <Carousel text={title} type={type} />
       ))}
