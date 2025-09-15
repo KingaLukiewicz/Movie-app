2076963a30e92cff06551ae4d5bce50fac71e288
KingaLukiewicz <kingal@itbs.pl>
2025-09-15 21:28:50 +0200

    display users' reviews on movie page

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -1,10 +1,51 @@
 import './MoviePage.css';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Carousel from '../components/Carousel';
-import { TMDB_TYPE, TMDB_ID_URL } from '../constants';
+import { TMDB_TYPE, TMDB_ID_URL, API_TOKEN } from '../constants';
+import Slider from 'react-slick';
+import axios from 'axios';
+import { REVIEW_SLIDER } from '../constants';
+import ReviewBox from '../components/ReviewBox';
+
+export type Review = {
+  id: string;
+  author: string;
+  rating: number;
+  content: string;
+  avatar_path?: string;
+};
+
+const mapReview = (item: any): Review => ({
+  id: item.id,
+  author: item.author,
+  rating: item.author_details.rating,
+  content: item.content,
+  avatar_path: item.author_details.avatar_path
+});
 
 function MoviePage() {
   const { movie_id } = useParams<{ movie_id: string }>();
+  const [reviews, setReviews] = useState<Review[]>([]);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const url = `${TMDB_ID_URL}/movie/${movie_id}/reviews?language=en-US`;
+        const response = await axios.get(url, {
+          headers: {
+            Authorization: `Bearer ${API_TOKEN}`,
+            accept: 'application/json'
+          }
+        });
+        setReviews(response.data.results.map((i: any) => mapReview(i)));
+      } catch (error) {
+        console.error('Failed to fetch', error);
+      }
+    };
+    fetchReviews();
+  }, []);
+
   return (
     <div className="MoviePage">
       <div className="Details">
@@ -20,6 +61,13 @@ function MoviePage() {
       </div>
       <div className="Reviews">
         <h2>Reviews</h2>
+        <div className="ReviewSlider">
+          <Slider {...REVIEW_SLIDER}>
+            {reviews.map((review) => (
+              <ReviewBox key={review.id} review={review} />
+            ))}
+          </Slider>
+        </div>
       </div>
       <div className="Recomendations">
         <h2>Recomendations</h2>
